a1886dd1a3198c86fa4abe6d7129fbde78c88cb8
Robson Sabino <robsonsabinof@gmail.com>
2024-09-09 14:29:35 -0300

    mais um

--- VETORBIDIMENSIONAL/104.c
+++ VETORBIDIMENSIONAL/104.c
@@ -4,7 +4,7 @@
 int main()
 {
     setbuf(stdout, NULL);
-    int cordenadas = 1, jogodaVelha[3][3], linha = 0, coluna = 0, jogador = 0;
+    int cordenadas = 1, jogodaVelha[3][3], linha = 0, coluna = 0, jogador = 0, fim = 0;
     for (int i = 0; i < 3; i++)
         for (int j = 0; j < 3; j++)
             jogodaVelha[i][j] = 2;
@@ -24,6 +24,7 @@ int main()
         printf("]\n");
     }
     cordenadas = 1;
+    int f = 0;
     for (int d = 0; d < 9; d++)
     {
 
@@ -41,17 +42,6 @@ int main()
             jogodaVelha[linha - 1][coluna - 1] = 0;
             jogador = 0;
         }
-        if (jogodaVelha[0][0] < 2 && jogodaVelha[0][0] == jogodaVelha[0][1] && jogodaVelha[0][0] == jogodaVelha[0][2])
-        {
-            if (jogador = 0)
-            {
-                printf("O jogador 1 venceu");
-            }
-            else
-            {
-                printf("O jogador 2 venceu");
-            }
-        }
         printf("  1 2 3\n");
         for (int i = 0; i < 3; i++)
         {
@@ -65,8 +55,32 @@ int main()
             cordenadas = cordenadas + 1;
             printf("]\n");
         }
-        cordenadas = 1;
+        for (int j = 0; j < 2; j++)
+        {
+            if (jogodaVelha[0][0] == j && jogodaVelha[0][1] == j && jogodaVelha[0][2] == j ||
+                jogodaVelha[1][0] == j && jogodaVelha[1][1] == j && jogodaVelha[1][2] == j ||
+                jogodaVelha[2][0] == j && jogodaVelha[2][1] == j && jogodaVelha[2][2] == j ||
+                jogodaVelha[0][0] == j && jogodaVelha[1][0] == j && jogodaVelha[2][0] == j ||
+                jogodaVelha[0][1] == j && jogodaVelha[1][1] == j && jogodaVelha[2][1] == j ||
+                jogodaVelha[0][2] == j && jogodaVelha[1][2] == j && jogodaVelha[2][2] == j ||
+                jogodaVelha[0][0] == j && jogodaVelha[1][1] == j && jogodaVelha[2][2] == j ||
+                jogodaVelha[0][2] == j && jogodaVelha[1][1] == j && jogodaVelha[2][0] == j)
+            {
+                if (jogador = 1)
+                {
+                    printf("O jogador 1 venceu\n\n");
+                    break;
+                }
+                else
+                {
+                    printf("O jogador 2 venceu\n\n");
+                    break;
+                }
+            }
+        }
+    cordenadas = 1;
     }
-    printf("\n\n");
-    return 0;
+printf("\nVelha, não houve ganhadores!!\n\n");
+printf("\n\n");
+return 0;
 }
