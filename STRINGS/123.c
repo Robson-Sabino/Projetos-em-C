d104dd5d8ba33a293b01a0c74ffcce16b41533e1
Robson Sabino <robsonsabinof@gmail.com>
2024-10-16 19:42:43 -0300

    mais um commit

---
+++ STRINGS/123.c
@@ -0,0 +1,19 @@
+// Inversor e trocador de letras
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <strlen.h>
+
+int main()
+{
+    char palavra[10];
+    int maior = 0;
+    int tamanho = strlen(palavra);
+    printf("Digite a palavra:\n");
+    scanf("%s", palavra);
+
+
+
+    return 0;
+}
